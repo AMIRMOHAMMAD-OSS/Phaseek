2db7c7ecf3ddd5be93d722569e5b8ac6fc526d9d
AmirMohammad MohammadHosseini <140107453+AMIRMOHAMMAD-OSS@users.noreply.github.com>
2025-01-01 22:24:02 +0330

    Update XGBoost.py

--- Functions/XGBoost.py
+++ Functions/XGBoost.py
@@ -1,6 +1,6 @@
 import pickle
 class XGBoost():
   def XGM():
-    with open('classi/xgb_model (1).pkl', 'rb') as f:
+    with open('/content/Phaseek/xgb_model (1).pkl', 'rb') as f:
       clf = pickle.load(f)
     return clf

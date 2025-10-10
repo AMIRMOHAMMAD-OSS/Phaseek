95393d5b6bc43d4ee51a5c2a01813ef6485471e2
AmirMohammad MohammadHosseini <140107453+AMIRMOHAMMAD-OSS@users.noreply.github.com>
2025-10-10 12:38:17 +0330

    Update FEGS_feature_extraction.py

--- Functions/FEGS_feature_extraction.py
+++ Functions/FEGS_feature_extraction.py
@@ -1,96 +1,134 @@
-def coordinate():
-    pt = [np.array([np.cos(i * 2 * np.pi / 20), np.sin(i * 2 * np.pi / 20), 1]) for i in range(20)]
-    P = np.vstack(pt)
-    V = [[pt[i] + (1 / 20) * (pt[j] - pt[i]) for j in range(20)] for i in range(20)]
-    return P, V
+
+import os
+import numpy as np
+from multiprocessing import Pool
+from tqdm import tqdm
+from scipy.io import loadmat
+from scipy.spatial.distance import pdist, squareform
+from scipy.sparse.linalg import eigs
+from Bio import SeqIO
 
 
-def FEGS(sequences,start_seq,end_seq):
-    P, V = coordinate()
-    if "fasta" in sequences:
-      sequences = [str(record.seq) for record in SeqIO.parse(sequences, "fasta")]
-    sequences = sequences[start_seq:end_seq]
-    l = len(sequences)
+class FEGSFeatureExtractor:
 
-    with Pool() as pool:
-        g_p = pool.starmap(GRS, [(seq, P, V) for seq in sequences])
-        EL = np.array([ME(i) for i in  tqdm([g for g_list in g_p for g in g_list])]).reshape(l,158)
+    def __init__(self, m_mat_path: str = "/content/classi/M.mat", processes: int | None = None):
+        self.m_mat_path = m_mat_path
+        self.processes = processes
+        mat = loadmat(self.m_mat_path)
+        self.M = mat["M"].flatten()
+        self.P, self.V = self._coordinate()
+        self.num_M = len(self.M)
 
-    char = 'ARNDCQEGHILKMFPSTWYV'
-    with Pool() as pool:
-        results = pool.map(SAD, [(seq, char) for seq in sequences])
-        FA = np.array([res[0] for res in results])
-        FD = np.array([res[1].flatten() for res in results])
+    @staticmethod
+    def _coordinate():
+        pt = [np.array([np.cos(i * 2 * np.pi / 20), np.sin(i * 2 * np.pi / 20), 1.0]) for i in range(20)]
+        P = np.vstack(pt) 
+        V = [[pt[i] + (1.0 / 20.0) * (pt[j] - pt[i]) for j in range(20)] for i in range(20)]
+        return P, V
 
-    FV = np.hstack((EL, FA, FD))
-    return FV
+    @staticmethod
+    def _GRS_static(seq: str, P: np.ndarray, V: list[list[np.ndarray]], M_flat):
 
+        l_seq = len(seq)
+        k = len(M_flat)
+        g = []
 
-def GRS(seq, P, V):
-    M = loadmat("/content/classi/M.mat")
-    M = M["M"].flatten()
-    l_seq = len(seq)
-    k = M.shape[0]
-    cha = 'ACDEFGHIKLMNPQRSTVWY'
-    g = []
+        for j in range(k):
+            c = [np.array([0.0, 0.0, 0.0])]
+            d = np.zeros(3, dtype=float)
+            y = None
 
-    for j in range(k):
-        DPC = np.zeros((20, 20))
-        c = [[0, 0, 0]]
-        d = np.zeros(3)
-        y = None
-
-        for i in range(l_seq):
-            x = np.array([seq[i] == aa for aa in M[j]]).astype(int)
-            if i == 0:
-                c.append(c[i] + np.dot(x, P))
-            else:
-                if not np.any(x):
-                    d = d * (i - 1) / i
-                    c.append(c[i] + [0, 0, 1] + d)
-                elif not np.any(y):
-                    d = d * (i - 1) / i
-                    c.append(c[i] + np.dot(x, P) + d)
+            motif = M_flat[j]
+            for i in range(l_seq):
+                x = np.array([seq[i] == aa for aa in motif], dtype=int)
+                if i == 0:
+                    c.append(c[i] + x @ P)  
                 else:
-                    d = d * (i - 1) / i + V[int(np.where(y)[0][0])][int(np.where(x)[0][0])] / i
-                    c.append(c[i] + np.dot(x, P) + d)
-            y = x
+                    if not np.any(x):
+                        d = d * (i - 1) / i
+                        c.append(c[i] + np.array([0.0, 0.0, 1.0]) + d)
+                    elif not np.any(y):
+                        d = d * (i - 1) / i
+                        c.append(c[i] + x @ P + d)
+                    else:
+                        prev_idx = int(np.where(y)[0][0])
+                        curr_idx = int(np.where(x)[0][0])
+                        d = d * (i - 1) / i + V[prev_idx][curr_idx] / i
+                        c.append(c[i] + x @ P + d)
+                y = x
 
-        g.append(np.vstack(c))
-    return g
+            g.append(np.vstack(c))  
+        return g
 
-def ME(W):
-    W = W[1:, :]
-    x = W.shape[0]
-    D = pdist(W)
-    E = squareform(D)
-    sdist = np.zeros((x, x))
+    @staticmethod
+    def _ME_static(W: np.ndarray) -> float:
 
-    for i in range(x):
-        for j in range(i, x):
-            if j - i == 1:
-                sdist[i, j] = E[i, j]
-            elif j - i > 1:
-                sdist[i, j] = sdist[i, j - 1] + E[j - 1, j]
+        W = W[1:, :]  
+        x = W.shape[0]
+        D = pdist(W)
+        E = squareform(D)
+        sdist = np.zeros((x, x), dtype=float)
 
-    sdist += sdist.T
-    sdd = sdist + np.diag(np.ones(x))
-    L = E / sdd
-    ME = eigs(L, k=1)[0][0] / x
-    return ME
+        for i in range(x):
+            for j in range(i, x):
+                if j - i == 1:
+                    sdist[i, j] = E[i, j]
+                elif j - i > 1:
+                    sdist[i, j] = sdist[i, j - 1] + E[j - 1, j]
 
-def SAD(args):
-    seq, a = args
-    len_seq = len(seq)
-    len_a = len(a)
-    AAC = np.zeros(len_a)
-    DPC = np.zeros((len_a, len_a))
+        sdist += sdist.T
+        sdd = sdist + np.diag(np.ones(x))
+        L = E / sdd
+        val = eigs(L, k=1)[0][0]
+        return float(np.real(val) / x)
 
-    c = [np.array([s == aa for s in seq]) for aa in a]
-    AAC = np.array([np.sum(c[i]) / len_seq for i in range(len_a)])
+    @staticmethod
+    def _SAD_static(args):
+        seq, a = args
+        len_seq = len(seq)
+        len_a = len(a)
 
-    if len_seq != 1:
-        for i in range(len_a):
-            for j in range(len_a):
-                DPC[i, j] = np.sum((np.roll(c[j], -1) * 2 - c[i]) == 1) / (len_seq - 1)
-    return AAC, DPC
+        c = [np.array([s == aa for s in seq], dtype=bool) for aa in a]
+        AAC = np.array([np.sum(c[i]) / max(len_seq, 1) for i in range(len_a)], dtype=float)
+
+        DPC = np.zeros((len_a, len_a), dtype=float)
+        if len_seq > 1:
+            for i in range(len_a):
+                for j in range(len_a):
+
+                    DPC[i, j] = np.sum((np.roll(c[j], -1).astype(int) * 2 - c[i].astype(int)) == 1) / (len_seq - 1)
+
+        return AAC, DPC
+
+    @staticmethod
+    def _load_sequences_any(sequences, start_seq: int, end_seq: int | None):
+
+        if isinstance(sequences, str) and "fasta" in sequences.lower():
+            seqs = [str(record.seq) for record in SeqIO.parse(sequences, "fasta")]
+        elif isinstance(sequences, (list, tuple, np.ndarray)):
+            seqs = list(sequences)
+        else:
+            raise ValueError("`sequences` must be a FASTA path or a list/array of sequences.")
+
+        return seqs[start_seq:end_seq]
+
+    def extract(self, sequences, start_seq: int = 0, end_seq: int | None = None) -> np.ndarray:
+
+        P, V, M_flat = self.P, self.V, self.M
+        sequences = self._load_sequences_any(sequences, start_seq, end_seq)
+        l = len(sequences)
+        with Pool(processes=self.processes) as pool:
+            g_p = pool.starmap(self._GRS_static, [(seq, P, V, M_flat) for seq in sequences])
+
+        EL_vals = [self._ME_static(W) for g_list in tqdm(g_p, desc="ME", leave=False) for W in g_list]
+        EL = np.array(EL_vals, dtype=float).reshape(l, self.num_M)
+
+        char = 'ARNDCQEGHILKMFPSTWYV' 
+        with Pool(processes=self.processes) as pool:
+            results = pool.map(self._SAD_static, [(seq, char) for seq in sequences])
+
+        FA = np.array([res[0] for res in results], dtype=float)               
+        FD = np.array([res[1].flatten() for res in results], dtype=float)      
+
+        FV = np.hstack((EL, FA, FD))
+        return FV
